891ac6c6f5ff4ad066d574b660a0c730e9d6f1ea
Kamil Kamiński <kkaminski9311@gmail.com>
2023-05-13 02:14:02 +0200

    tracks in reverse directions

--- js/utils.js
+++ js/utils.js
@@ -6,3 +6,10 @@ export function instantiateTemplate(id) {
     const template = document.querySelector(id);
     return template.content.cloneNode(true);
 }
+
+export const qs = document.querySelector.bind(document);
+
+export const playAudio = (audio) => {
+    audio.currentTime = 0;
+    audio.play();
+};
