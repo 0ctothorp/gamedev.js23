6dd1d37c94c1205b1e4f79edc0684a0fd00136f8
Kamil Kamiński <kkaminski9311@gmail.com>
2023-05-29 18:01:03 +0200

    hard mode working

---
+++ js/consts.js
@@ -0,0 +1,14 @@
+window.DIFFICULTY_CONFIGS = {
+    NORMAL: {
+        punishOnMistake: false,
+    },
+    HARD: {
+        punishOnMistake: true,
+    },
+    INSANE: {
+        punishOnMistake: true,
+    },
+    IMPOSSIBLE: {
+        punishOnMistake: true,
+    },
+};
