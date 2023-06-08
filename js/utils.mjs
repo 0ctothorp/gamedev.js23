e62e90f67c0cff8df34cfeb4535675683fad5ecb
Kamil Kamiński <kkaminski9311@gmail.com>
2023-06-08 02:21:54 +0200

    change some styles; tweak speed progression a little; change "you lost" modal

--- js/utils.mjs
+++ js/utils.mjs
@@ -13,3 +13,15 @@ export const playAudio = (audio) => {
     audio.currentTime = 0;
     audio.play();
 };
+
+/** Map<queryString, DOMNode> */
+const DOM_NODES = new Map();
+
+export function getEl(selector) {
+    const hasCached = DOM_NODES.has(selector);
+    if (!hasCached) {
+        const el = qs(selector);
+        DOM_NODES.set(selector, el);
+    }
+    return DOM_NODES.get(selector);
+}
