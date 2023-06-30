df7e54379b9b3a1f95bf08b3fd8afa678484f646
Kamil Kamiński <kkaminski9311@gmail.com>
2023-07-01 01:56:29 +0200

    fix highscore info; separate highscore for every difficulty; crazygames updates;

--- js/consts.js
+++ js/consts.js
@@ -1,14 +1,23 @@
 window.DIFFICULTY_CONFIGS = {
     NORMAL: {
+        name: "normal",
         punishOnMistake: false,
     },
     HARD: {
+        name: "hard",
         punishOnMistake: true,
     },
     INSANE: {
+        name: "insane",
         punishOnMistake: true,
     },
     IMPOSSIBLE: {
+        name: "impossible",
         punishOnMistake: true,
     },
+};
+
+window.LS_KEYS = {
+    HS_NORMAL: "__time_flies__highscore_normal",
+    HS_HARD: "__time_flies__highscore_hard",
 };
