bb553b4da1bf928d6a7f05fa03ba9d40b52fadbc
Kamil Kamiński <kkaminski9311@gmail.com>
2023-10-11 22:58:43 +0200

    better round announcement

--- js/utils.mjs
+++ js/utils.mjs
@@ -7,27 +7,13 @@ export function instantiateTemplate(id) {
     return template.content.cloneNode(true);
 }
 
-export const qs = document.querySelector.bind(document);
-export const qsa = document.querySelectorAll.bind(document);
-
 export const playAudio = (audio) => {
     audio.currentTime = 0;
     audio.play();
 };
 
-/** Map<queryString, DOMNode> */
-const DOM_NODES = new Map();
-
-export function $(selector, multiple = false) {
-    const hasCached = DOM_NODES.has(selector);
-    if (!hasCached) {
-        const el = multiple ? qsa(selector) : qs(selector);
-        DOM_NODES.set(selector, el);
-    }
-    return DOM_NODES.get(selector);
-}
-
-export const $$ = (selector) => $(selector, true);
+export const $ = document.querySelector.bind(document);
+export const $$ = document.querySelectorAll.bind(document);
 
 export const showEl = (el) => el.classList.add("active");
 export const hideEl = (el) => el.classList.remove("active");
