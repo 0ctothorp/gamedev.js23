8d07c7ccc85df5ca7e1987e9a061de4c6d90d50a
Kamil Kamiński <kkaminski9311@gmail.com>
2023-12-09 00:30:26 +0100

    update powershell zip script

--- zip.ps1
+++ zip.ps1
@@ -1,4 +1,4 @@
 # compress dist contents to zip and force write if ./zipped.zip already exists
-Compress-Archive -Force -Path .\index.html, .\sdk.html, .\styles.css, .\reset.css, .\assets, .\js -DestinationPath .\package.zip
+Compress-Archive -Force -Path .\index.html, .\css, .\assets, .\js -DestinationPath .\package.zip
 # prints file size in KB
 Write-Host((Get-Item .\package.zip).length / 1KB)
